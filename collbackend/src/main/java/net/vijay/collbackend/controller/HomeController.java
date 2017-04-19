0e6322a8b6de460c991761b287448eedbb9ffc19
Vijay Tripurari <vijay.tripurari@gmail.com>
2017-04-19 23:40:53 +0530

    this is my first commit

---
+++ collbackend/src/main/java/net/vijay/collbackend/controller/HomeController.java
@@ -0,0 +1,32 @@
+package net.vijay.collbackend.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView LandingPage() {
+		System.out.println("In Home page");
+	
+		return new ModelAndView("index");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
